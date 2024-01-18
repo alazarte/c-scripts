56fafaf13c77b40eac7d568f9c514de299487fe4
alazarte <inbox@alazarte.com>
2024-01-18 10:23:05 -0300

    format seconds to hours/mins/seconds

--- session.c
+++ session.c
@@ -10,23 +10,55 @@ void read_file(char *filename, char *buffer)
 	while ((*buffer++ = getc(f)) != EOF) ;
 }
 
-int main()
+char *parse_seconds(size_t seconds, char *buffer) {
+	size_t hours = 0, minutes = 0;
+	while(seconds >= 3600) {
+		hours++;
+		seconds -= 3600;
+	}
+	while(seconds >= 60) {
+		minutes++;
+		seconds -= 60;
+	}
+
+	size_t len = 0;
+	if (hours > 0)
+		len = sprintf(buffer + len, "%ldh ", hours);
+	if (minutes > 0)
+		len = sprintf(buffer + len, "%ldm ", minutes);
+	if (seconds > 0)
+		len = sprintf(buffer + len, "%lds ", seconds);
+
+	return buffer;
+}
+
+int main(int argn, char **argv)
 {
 	char *session_state_filepath = "/tmp/.session_state";
 	char *session_start_filepath = "/tmp/.session_start";
-	int seconds = 0;
-	char buffer[100] = { 0 };
+	size_t seconds = 0;
+	char file_buffer[100] = { 0 };
+	char output_buffer[100] = { 0 };
+
+	if (argn == 2) {
+		FILE *f = fopen(session_start_filepath, "w+");
+		fprintf(f, "%s", argv[1]);
+		fclose(f);
+		return 0;
+	}
 
 	for (;;) {
 		if (access(session_start_filepath, F_OK) == 0) {
-			read_file(session_start_filepath, buffer);
+			read_file(session_start_filepath, file_buffer);
 			remove(session_start_filepath);
-			seconds = atoi(buffer);
+			seconds = atoi(file_buffer);
 		}
 
 		if (seconds > 0) {
 			FILE *f = fopen(session_state_filepath, "w+");
-			fprintf(f, "%d %s", seconds, "🔴");
+			parse_seconds(seconds, output_buffer);
+			printf("output: %s\n", output_buffer);
+			fprintf(f, "%s %s", output_buffer, "🔴");
 			fclose(f);
 			seconds--;
 		} else {
